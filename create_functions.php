b4343ca51cddb45501c66d09007ba254a2de0ae0
reebok <reebok@localhost>
2015-02-19 17:49:16 +0200

    функции отрисовки таблицы и форм вынесены в отдельный файл create_functions.php Добавлен скрипт javascript во внешнем файле (JS/script.js) проведена неудачная попытка через скрипт отрисовывать страницу через GET метод. При этом переменные $_POST обнуляются

---
+++ create_functions.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body BGCOLOR="#E6E8fA" text="#000002" link="#666699" vlink="#666699" alink = "#333366" BACKGROUND = "background.jpg">
+
+
+<?php
+
+   function generate_table($dbc,$columns,$where='', $order_by, $sort, $limit=4){
+      
+      $query = "SELECT $columns FROM t1 $where  ORDER BY $order_by $sort LIMIT $limit";
+      if (!$data = mysqli_query($dbc, $query))
+	    echo 'query error - '.$query;
+//       echo "<br> $query <br>\n";
+      $column = explode(",",$columns);
+
+      echo "<table border=1>\n";
+// first row of table
+      echo "  <tr>\n";
+      foreach($column as $i) { 
+// variant with javascript
+ 	  echo "<td> &nbsp&nbsp&nbsp&nbsp<b>
+ 		<a id=\"$i\"
+ 		  href=\"javascript:void(0)\" onclick=\"document.forms.select_form.submit();
+ 		  return false;\"> $i
+ 		</a>
+ 		</b>&nbsp&nbsp&nbsp&nbsp</td>\n";
+
+// variant with $_GET
+	  /*echo "<td> &nbsp&nbsp&nbsp&nbsp<b>";
+// 	  echo "<a href=\"index.php?order_by=$i\" onclick=\"count_rabbit()  return false;\"> $i </a>"
+	  echo "<a href=\"index.php?order_by=$i\" onclick=\"count_rabbit()\"> $i </a>";
+	  */echo "</b>&nbsp&nbsp&nbsp&nbsp</td>\n";
+       } 
+// 	    first row values as simple text
+// 	    echo "<td><b> $i </b></td>"; }
+      echo "  </tr>\n";
+
+// Loop through the array of user data, formatting it as HTML table
+// $odd_value -variable for difrent row color fill   
+      $odd_value = 1;
+      while ($row = mysqli_fetch_array($data)) {
+	  if (($odd_value % 2) == 0 )
+	       echo '   <tr bgcolor="BDB76B"> ';
+	  else echo '   <tr> ';
+	  $odd_value = $odd_value + 1;
+
+	  // выравнивание колонок по центру кроме name & country
+ 	  foreach($column as $i) 
+		if ( $i == 'name' OR $i == 'country' )
+		    echo '<td>'. htmlspecialchars( $row[$i] ).'</td> ';
+
+///////////////// uncomment next rows for highlighting profit colums /////////////////////
+//											//
+// 		else									//
+// 		   if ( $i == 'profit_battle' OR $i == 'profit_battle_premium' )	//
+// 			echo '<td align="center" bgcolor="mediumaquamarine">'. 		//
+//			      htmlspecialchars( $row[$i] ).' </td>'; 			//
+//											//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+		else
+		    echo '<td align="center">'. htmlspecialchars( $row[$i] ).'</td>'; 
+	  echo "</tr>\n";
+       }
+
+      echo "</table><br />\n";
+      return (0);
+  }
+
+ function MakeRadioButtons($name,$array_string,$vertical = False){
+	if (!is_array ($array_string))
+	    return ("make_radio_buttons: values argument must be an array");
+    	$str='';
+	foreach($array_string as $rb_name){
+ 	  $rb_name = htmlspecialchars( substr($rb_name,1,(strlen($rb_name)-2)) );
+	  $str = $str."<input type=\"radio\" name=\"$name\" value=\"$rb_name\" />$rb_name";
+	  if($vertical) 
+	      $str = $str.'<br />';
+	}
+	unset($rb_name);
+	return($str);
+     }
+
+ function MakePopupMenu($name,$values,$selected){
+	if (!is_array($values) )
+	    return ("make_popup_menu: values argument must be an array");
+
+     	$str = "<select name=\"$name\">\n";
+	foreach($values as $val){
+	    $val = htmlspecialchars($val);
+	    if ($val == $selected)  
+		$str = $str."<option selected value=\"$val\">$val</option>\n";
+	    else 
+		$str = $str."<option value=\"$val\">$val</option>\n";
+	}
+/* variant whith "for" operator:
+	for ($i = 0; $i < count($values); $i++){
+  	    $values[$i] = htmlspecialchars($values[$i]);
+	    if ($i == $selected)
+		$str = $str."<option selected value=\"$values[$i]\">$values[$i]</option>\n";
+	    else 
+		$str = $str."<option value=\"$values[$i]\">$values[$i]</option>\n";
+	}
+*/
+	$str = $str."</select><br />\n";
+	return($str);
+     }
+
+ function MakeCheckBoxsInTable($name, $values, $checked_boxes, $box_per_cell=2, $vertical=True){
+	if ( !is_array($values) )
+	    return ("make_check_boxs: values argument must be an array");
+	if ( !is_array($checked_boxes) )
+	    return ("make_check_boxs: checked_boxs argument must be an array");
+
+	$string_result = "<table border=0>\n<tr>\n   <td nowrap>\n";
+	$i = 0;
+        foreach($values as $cb_name){
+	    $i = $i+1;
+	    $string_result .= "<input type=\"checkbox\" name=\"$cb_name\" value=\"$cb_name\" ";
+			    // отметка значений по умолчанию
+	    if(in_array($cb_name,$checked_boxes) )
+		  $string_result .= "checked=\"checked\"";
+	    $string_result .= "/>$cb_name";
+	    if($vertical) 
+		  $string_result .= "<br />\n";
+	    if ( (($i % $box_per_cell) == 0) AND ($i < count($values)) )
+		  $string_result .= "\n   </td>\n   <td nowrap >\n";	
+	}
+	unset($cb_name);
+	$string_result.="   </td>\n</tr>\n</table>";
+	return($string_result);
+    }
+
+  function CreateWhere($arr){
+      if (! is_array($arr) ) return('function CreateWhere error: argument must be an array!');
+      if ( count($arr) == 0) return('');
+
+      $string='WHERE ';
+      foreach($arr as $key => $val)
+	  $string .= "$key='$val' AND ";
+
+      $string = substr($string, 0, strlen($string)-4 );
+      return($string);
+  }
+
+
+?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -2,135 +2,33 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Wold Of Tanks farm table page</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="./JS/script.js"></script>   
+    <title>Wold Of Tanks farm table page</title>
 </head>
+
 <body BGCOLOR="#E6E8fA" text="#000002" link="#666699" vlink="#666699" alink = "#333366" BACKGROUND = "background.jpg">
 
-<p>Hello.</p>
-<p>The current date is <?php print (date ("D M d H:i:s T Y")); ?>.</p>
-<p>На этой странице Вашему вниманию представлена таблица доходности всей техники игры
-"World of Tanks". Любителям танковых сражений эта таблица поможет проанализировать различные показатели
-боевых машин и целесообразность преобретения той или иной машины, в зависимости от целей, которые поставил себе
-игрок</p>
-<hr />
+  <p>The current date is <?php print (date ("D M d H:i:s T Y")); ?>.</p>
+  <hr />
+      Что это за сайт, что за цифры и зачем все это нужно  =>
+      <a href="about.htm">  ccылка </a>
+  <p>На этой странице представлена таблица доходности всей техники игры
+  "World of Tanks". Любителям танковых сражений эта таблица поможет проанализировать различные показатели
+  боевых машин и целесообразность преобретения той или иной машины, в зависимости от целей, которые поставил себе
+  игрок</p>
+  <hr />
+
 
 <?php
 
-  function generate_table($dbc,$columns,$where='', $order_by, $sort, $limit=4){   
-      
-      $query = "SELECT $columns FROM t1 $where  ORDER BY $order_by $sort LIMIT $limit";      
-      if (!$data = mysqli_query($dbc, $query))
-	  echo 'query error - '.$query;
-      echo "<br>$query";
-      $column = explode(",",$columns);
-
-      echo "<table border=1>\n";
-// first row of table
-      echo '<tr>';
-      foreach($column as $i) { echo "<td><b> $i </b></td>"; }
-      echo "</tr>\n";
-
-// Loop through the array of user data, formatting it as HTML table
-      while ($row = mysqli_fetch_array($data)) {
-	  echo '<tr>';
-
-// выравнивание колонок по центру кроме name & country
- 	  foreach($column as $i) 
-		if ( $i == 'name' OR $i == 'country' )
-		   echo '<td>'. htmlspecialchars( $row[$i] ).' </td>';  
-		else
-		   echo '<td align="center">'. htmlspecialchars( $row[$i] ).' </td>'; 
-	  echo "</tr>\n";
-      }
-
-      echo "</table><br />\n";
-      return (0);
-  }
- function MakeRadioButtons($name,$array_string,$vertical = False){
-	if (!is_array ($array_string))
-	    return ("make_radio_buttons: values argument must be an array");
-    	$str='';
-	foreach($array_string as $rb_name){
- 	  $rb_name = htmlspecialchars( substr($rb_name,1,(strlen($rb_name)-2)) );
-	  $str = $str."<input type=\"radio\" name=\"$name\" value=\"$rb_name\" />$rb_name";
-	  if($vertical) 
-	      $str = $str.'<br />';
-	}
-	unset($rb_name);
-	return($str);
-     }
-
- function MakePopupMenu($name,$values,$selected){
-	if (!is_array($values) )
-	    return ("make_popup_menu: values argument must be an array");
-
-     	$str = "<select name=\"$name\">\n";
-	foreach($values as $val){
-	    $val = htmlspecialchars($val);
-	    if ($val == $selected)  
-		$str = $str."<option selected value=\"$val\">$val</option>\n";	      
-	    else 
-		$str = $str."<option value=\"$val\">$val</option>\n";
-	}
-/*
-	for ($i = 0; $i < count($values); $i++){
-  	    $values[$i] = htmlspecialchars($values[$i]);
-	    if ($i == $selected)  
-		$str = $str."<option selected value=\"$values[$i]\">$values[$i]</option>\n";	      
-	    else 
-		$str = $str."<option value=\"$values[$i]\">$values[$i]</option>\n";
-	}*/
-	$str = $str."</select><br />\n";	
-	return($str);
-     }
-
- function MakeCheckBoxsInTable($name, $values, $checked_boxes, $box_per_cell=2, $vertical = True){
-	if (!is_array($values) )
-	    return ("make_check_boxs: values argument must be an array");
-	if (!is_array($checked_boxes) )
-	    return ("make_check_boxs: checked_boxs argument must be an array");
-
-	$string_result = "<table border=0>\n<tr>\n   <td nowrap>\n";
-	$i=0;
-        foreach($values as $cb_name){
-	  $i = $i+1;
-	  $string_result .= "<input type=\"checkbox\" name=\"$cb_name\" value=\"$cb_name\" ";
-		  // отметка значений по умолчанию
-	  if(in_array($cb_name,$checked_boxes) )
-		$string_result .= "checked=\"checked\"";   
-	  $string_result .= "/>$cb_name";
-	  if($vertical) $string_result .= "<br />\n";
- 	  if ( (($i % $box_per_cell) == 0) AND ($i < count($values)) ) 
- 		$string_result .= "\n   </td>\n   <td nowrap >\n";
-	
-	}	
-
-	unset($cb_name);
-	$string_result.="   </td>\n</tr>\n</table>";
-	return($string_result);
-    }
-  function CreateWhere($arr){
-      if (! is_array($arr) ) return('function CreateWhere error: argument must be an array!');
-      if ( count($arr) == 0) return('');
-
-      $string='WHERE ';
-      foreach($arr as $key => $val)
-	  $string .= "$key='$val' AND ";
-
-      $string = substr($string, 0, strlen($string)-4 );
-      return($string);
-  }
-
-
-
-
 //-------------------------- Main code -------------------------------------
-  require_once('connect.php');		// data for connect to MySQL database
+  require_once('connect.php');			// data for connect to MySQL database
+  require_once('create_functions.php');		// functions for create forms and tables
 
   if (!$dbc=mysqli_connect(DB_HOST,DB_USER, DB_PASSWORD, DB_NAME))
       echo ' Ошибка соединеиния с базой';
-  
+
 
 // ------------------------ Default values ---------------------------------
   $columns  = 'name,country,level,xp,profit_battle,profit_battle_premium';
@@ -141,7 +39,6 @@
   $type	    = 'all';
   $country2 = 'all';
 //--------------------------------------------------------------------------
-
 
 // выбор выводимыx колонок
   $checked_boxs = explode(",",$columns);
@@ -169,82 +66,84 @@
 	  $pop_menu_country_values = preg_replace('/\'/','',$pop_menu_country_values);
 	  array_unshift($pop_menu_country_values, 'all');
       }
-  
-  }
- 
 
-
-//================ Grab the profile data from the POST ==================
-//=======================================================================
-  if (isset($_POST['submit'])) { 
-      $columns = '';
-      $limit   = $_POST['limit'];
-
-      if ($_POST['sort'] == 'по убыванию')
-	   $sort = 'DESC';      
-      else $sort = '';
-
-      foreach($check_boxs as $val){
-	  if (isset($_POST[$val]) && $_POST[$val] == $val)
-	      $columns .=",$val";	
-      }
-      $columns = substr($columns, 1);  // delete first "," in string
-
-// формирование предиката WHERE для запроса
-      $_array = array();		// array of filters
-
-      if (isset($_POST['type_popUp']) AND $_POST['type_popUp'] != 'all'){
- 	 $_array['type'] = $_POST['type_popUp'];
-		   $type = $_POST['type_popUp'];
-      }
-      if (isset($_POST['country2']) AND $_POST['country2'] != 'all'){
-	 $_array['country'] = $_POST['country2'];
-		  $country2 = $_POST['country2'];
-      }
-      var_dump($_array);
-      $where = CreateWhere($_array);
   }
 
-  $checked_boxs = explode(",",$columns);
-         
-// =======================================================================
+
+// ================== Grab data from the $_POST ============================
+// =========================================================================
+      if (isset($_POST['limit']) ) { 
+	  $columns = '';
+	  $limit   = $_POST['limit'];
+
+	  if ($_POST['sort_popUp'] == 'по убыванию'){
+		$sort = '';		
+		$_POST['sort_popUp'] = 'по возрастанию';
+	  }
+	  else 
+		$sort = 'DESC';  	
+
+	  foreach($check_boxs as $val){
+	      if (isset($_POST[$val]) && $_POST[$val] == $val)
+		  $columns .=",$val";
+	  }
+	  $columns = substr($columns, 1);  // delete first "," in string
+
+// Формирование предиката WHERE для запроса и запоминание выбранных 
+// значений для <select checked=... > в PopUpMenu()
+	  $_array = array();		   // array of WHERE filters
+
+	  if (isset($_POST['type_popUp']) AND $_POST['type_popUp'] != 'all'){
+	    $_array['type'] = $_POST['type_popUp'];
+		      $type = $_POST['type_popUp'];
+	  }
+
+	  if (isset($_POST['country2']) AND $_POST['country2'] != 'all'){
+	    $_array['country'] = $_POST['country2'];
+		     $country2 = $_POST['country2'];
+	  }
+      
+	  $where = CreateWhere($_array);
+      }
+
 // =======================================================================
 //		 		Global filters
 // limits value for output pop_menu 'filter amount rows in data table'
-    if (!empty($limit)) 
-	  $pop_menu_limit_values = array($limit,10,20,40);
-    else  $pop_menu_limit_values = array(5,40,60);
-		
+      $pop_menu_limit_values = array(10,20,40);
 // sort order for output pop_menu 'DESC INC'
-  $pop_menu_sorting_value = array('по убыванию','по возрастанию');
+      $pop_menu_sorting_value = array('по убыванию','по возрастанию');
+      if ($sort == 'DESC') 
+	   $sorting_value_selected = 'по убыванию';
+      else $sorting_value_selected = 'по возрастанию';
 
-// type values for pop_menu 'filter of types'
+      // array of columns for output
+      $checked_boxs = explode(",",$columns);
 
 ?>
-<!--*********** таблица фильтров колонок//table filter's columns ********* -->
+<!--*********** таблица фильтров колонок//table filter's columns ********** -->
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-   <input type="submit" value="Применить" name="submit" /><br />
-   <table border=0>
+<form name="select_form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   <input type="submit" value="Применить" name="submit_button"/><br />
+   <table border=0 >
       <tr>
 	  <td align="center">строк на страницу:<br />
-	      <?php echo MakePopupMenu('limit',$pop_menu_limit_values,$limit)?>
+	      <?php echo MakePopupMenu('limit', $pop_menu_limit_values, $limit)?>
 	  </td>
 	  <td align="center">порядок сортировки:<br />
-	      <?php echo MakePopupMenu('sort',$pop_menu_sorting_value,$pop_menu_sorting_value[0]) ?>
+	      <?php echo MakePopupMenu('sort_popUp', $pop_menu_sorting_value, $sorting_value_selected) ?>
 	  </td>
 	  <td align="center">виды танков:<br />
-	      <?php echo MakePopupMenu('type_popUp',$pop_menu_type_values,$type) ?>
+	      <?php echo MakePopupMenu('type_popUp', $pop_menu_type_values, $type) ?>
 	  </td>
 	  <td align="center">cтрана:<br />
-	      <?php echo MakePopupMenu('country2',$pop_menu_country_values,$country2) ?>
+	      <?php echo MakePopupMenu('country2', $pop_menu_country_values, $country2) ?>
 	  </td>
 
       </tr>
     </table>
 
 <!-- Фильтры колонок под спойлером
-// Checkboxes fiters under spoiler   -->
+// Checkboxes fiters under spoiler      -->
     <div class="spoil">
     <div class="smallfont">
     <hr />
@@ -260,25 +159,41 @@
     <div style="display: none;">
     Select columns for display/ Выберите колонки для отображения
 	  <?php
-	    echo MakeCheckBoxsInTable('columns',$check_boxs, $checked_boxs,5);
+	    echo MakeCheckBoxsInTable('columns', $check_boxs, $checked_boxs, 5);
 	  ?>     
     </div>
     </div>
     </div>
     <hr />
-</form>
 
-<!--******************************************************************* -->
+
+<!--****************** Output Main Table *********************************************** -->
 
 <?php
-
   echo '<br /> *Цена на премиумную технику указана в игровом золоте';
-  generate_table($dbc,$columns,$where,$order_by, $sort, $limit);
+
+if (isset($_GET['order_by']) ){
+    $order_by = $_GET['order_by'];
+    echo '<br />переменная $_GET определена: '.$order_by;
+  }
+
+echo '<br /> режим сортировки: '.$order_by;
+  generate_table($dbc, $columns, $where, $order_by, $sort, $limit);
+  
+
+//   $query = "SELECT length(name) FROM t1 ORDER BY level LIMIT $limit";
+//       if (!$data = mysqli_query($dbc, $query))
+// 	    echo 'query error - '.$query;
+//   while ($row = mysqli_fetch_array($data)) 
+//   echo "<br>$row[0]";
 
 
   mysqli_close($dbc);
-  
 ?>
 
-</body>
+      </form>
+
+<a href="javascript:void(0)" onclick="count_rabbits()"> click for javascript</a> 
+<!-- <a href="http://localhost/wot/index.php?order_by=level" onclick="count_rabbits()"> click for javascript</a> -->
+  </body>
 </html>
